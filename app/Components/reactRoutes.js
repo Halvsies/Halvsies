c79e35fe347d6b7718137cc632c6df13699eaae8
IamZombiez <dereksiu216@gmail.com>
2017-07-01 15:06:41 -0700

    module pathing fixed

--- app/Components/reactRoutes.js
+++ app/Components/reactRoutes.js
@@ -1,30 +1,20 @@
 import React from "react";
 import {render} from "react-dom";
-import {Router, Route, browserHistory} from "react-router";
+import {Router, Route, browserHistory, hashHistory, IndexRoute} from "react-router";
 
-var hashHistory = router.hashHistory;
-// Include the IndexRoute (catch-all route)
-var IndexRoute = router.IndexRoute;
+import {Main} from "./Main.js"
 
-import {Main} from "Main"
 // import {Login} from "main"
 // import {Home} from "./components/Home"
 // import {Buy} from "./components/Buy"
 // import {Sell} from "./components/Sell"
 
 class reactRoutes extends React.Component {
-  constructor(props) {
-    super(props);
-  }
 
     render() {
       return (
         <Router history={browserHistory}>
           <Route path={"/"} component={Main}/>
-          // <Route path={"/"} component={Login}/>
-          // <Route path={"/Home"} component={Home}/>
-          // <Route path={"/Buy"} component={Buy}/>
-          // <Route path={"/Sell"} component={Buy}/>
         </Router>
         );
       }
