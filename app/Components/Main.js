38f5798b7cc44219694b431c2cad9892609b25ed
Chris French <clfrench84@gmail.com>
2017-07-04 10:43:30 -0700

    worked on searchbar and main view

--- app/Components/Main.js
+++ app/Components/Main.js
@@ -1,19 +1,43 @@
 var React = require('react');
 var axios = require('axios');
-
+var Router = require('react-router');
+var Navbar = require('react-bootstrap').Navbar;
+var FormGroup = require('react-bootstrap').FormGroup;
+var FormControl = require('react-bootstrap').FormControl;
+var Button = require('react-bootstrap').Button;
+var Nav = require('react-bootstrap').Nav;
+var NavItem = require('react-bootstrap').NavItem;
 
 var helpers = require('./utils/helpers.js');
 
 var Main = React.createClass({
 
-  render: function() {
-    return (
+  render: function(){
 
-      <div className="container">
+    return(
 
-      <h1> hello </h1>
+      <Navbar>
+       <Navbar.Header>
+         <Navbar.Brand>
+           <a href="#">Halvsies</a>
+         </Navbar.Brand>
+         <Navbar.Form pullLeft>
+           <FormGroup>
+             <FormControl type="text" placeholder="Search" />
+           </FormGroup>
+           {' '}
+           <Button type="submit"><i className="fa fa-search" aria-hidden="true"></i></Button>
+         </Navbar.Form>
+         <Navbar.Toggle />
+       </Navbar.Header>
+       <Navbar.Collapse>
+       <Nav pullRight>
+        <NavItem eventKey={1} href="#">Buy</NavItem>
+        <NavItem eventKey={2} href="#">Sell</NavItem>
+       </Nav>
+       </Navbar.Collapse>
+      </Navbar>
 
-      </div>
     )
   }
 });
