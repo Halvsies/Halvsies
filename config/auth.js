58e9c8e004f2594d7b57bd7061cbbb50d70f24f9
Tony Tran <tcbtran@calvisitor-10-105-146-104.calvisitor.1918.berkeley.edu>
2017-07-05 21:20:23 -0700

    oauth2 branch

---
+++ config/auth.js
@@ -0,0 +1,10 @@
+// Exposing Google OAuth2 to use with application
+module.exports = {
+
+	'googleAuth' : {
+		'clientID':  '566355974110-pdea0tdciu9gi0ot03a01rdoh983fc02.apps.googleusercontent.com',
+		'clientSecret': 'Veas4SKb0B4CfXdQUPe5HP1O',
+		'callbackURL': 'http://localhost:8080/auth/google/callback'
+	}
+
+};
