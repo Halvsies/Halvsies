7b3be38ca674a71431e628ac03b4616502f392e6
victorwltsang <victorwltsang@gmail.com>
2017-07-17 19:40:09 -0700

    heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var bodyParser = require('body-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose');
 
-var Listing = require('./models/Listing.js');
+var Listing = require('./models/Listing');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
